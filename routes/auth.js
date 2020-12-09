57620660e6a333e2e35b0426be09a7dd268a4180
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-12-09 14:17:02 -0600

    Write user registration method on User class

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,6 @@ const ExpressError = require("../expressError");
 const User = require("../models/user");
 
 
-const bcrypt = require("bcrypt");
-const { BCRYPT_WORK_FACTOR } = require("../config");
-const db = require("../db");
-
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
@@ -28,21 +24,9 @@ const db = require("../db");
       if (!username || !password) {
         throw new ExpressError("Username, password, first name, last name, and phone number are required.", 400);
       }
-      // const results = await User.register({ username, password, firstName, lastName, phone});
-      // hash the password
-      const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
-      // save to database
-      const results = await db.query(`
-        INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
-        VALUES ($1, $2, $3, $4, $5, LOCALTIMESTAMP,CURRENT_TIMESTAMP)
-        RETURNING username`,
-        [username, hashedPassword, firstName, lastName, phone]
-      );
-      console.log("results.rows: ", results.rows);
-      console.log("results.rows[0]: ", results.rows[0]);
-      return res.json(results.rows[0]);
-      // const results = await User.register({ username, password, firstName, lastName, phone });
-      // return res.json(results.rows[0]);
+      const result = await User.register({ username, password, firstName, lastName, phone });
+      console.log("results from auth route: ", result);
+      return res.json(result);
     } catch (err) {
       console.log(err);
       if(err.code === '23505') {
