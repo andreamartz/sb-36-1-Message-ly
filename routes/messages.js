7f784049915bd3587534494e75324cabccb2151c
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-12-12 14:22:36 -0600

    Add route to get a specific message

--- routes/messages.js
+++ routes/messages.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const ExpressError = require("../expressError");
 const router = new express.Router();
+const Message = require("../models/message");
+const { ensureLoggedIn } = require("../middleware/auth");
 
-/** GET /:id - get detail of message.
+/******************************************
+ * 
+ * GET /:id - get detail of message.
  *
  * => {message: {id,
  *               body,
@@ -12,8 +17,25 @@ const router = new express.Router();
  *
  * Make sure that the currently-logged-in users is either the to or from user.
  *
- **/
+ ******************************************/
 
+router.get('/:id', 
+ensureLoggedIn, 
+async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.user;
+    const msg = await Message.get(id);
+    console.log("MSG: ", msg);
+    // make sure that the requesting user is either the 'to' or 'from' user
+    if (username != msg.from_user.username && username != msg.to_user.username) {
+      throw new ExpressError("Not authorized to view this message", 403);
+    }
+    return res.json({ message: msg });
+  } catch(err) {
+    next(err);
+  }
+});
 
 /** POST / - post message.
  *
